7d36ae47bc3a834adc98853d6e2d5f4cb50c8efc
recaptcha-19 <rahulpriyadarshan19@gmail.com>
2021-12-22 12:35:53 +0530

    Added files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+	name = "ionstruct",
+	version = "0.0.1",
+	author = "Rahul Priyadarshan R",
+	author_email = "rahulpriyadarshan19@gmail.com",
+	description = "A Python package to generate visualizations of TEC measurements in the ionosphere.",
+	packages = find_packages()
+	
+)
